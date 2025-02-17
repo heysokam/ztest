fa439f18e056f1f2c27a313604e3c301eb07c73c
sOkam <7308253+heysokam@users.noreply.github.com>
2025-02-17 07:10:26 +0100

    git: Add Package Info

---
+++ info.zig
@@ -0,0 +1,8 @@
+//:_____________________________________________________________________
+//  ztest  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU LGPLv3 or later  :
+//:_____________________________________________________________________
+const version     = "0.0.0";
+const name        = "ztest";
+const description = "z*test | Unit Testing Extensions for Zig";
+const author      = "heysokam";
+const license     = "LGPLv3-or-later";
